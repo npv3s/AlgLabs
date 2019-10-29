15f11b64e330bd145ad4f785a72b075f28350c57
Nikolai <npv3s@skopa.dev>
2019-10-29 12:32:58 +0300

    upd

--- lab6v5/main.cpp
+++ lab6v5/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    enum Menu {
+        olivie=150,
+        cesar=120,
+        blinchiki=80,
+        kompot=20,
+        bulochka=10
+    };
+    while (true) {
+        string item;
+        cout << "Введите название блюда (olivie,cesar,blinchiki,kompot,bulochka): ";
+        cin >> item;
+        if (item=="stop") { break;}
+        
+
+    }
     return 0;
 }
