ca21725bd24657f1ccf12e3de407f4e3dea25423
Nikolai <npv3s@skopa.dev>
2019-10-29 13:03:58 +0300

    upd

--- lab6v5/main.cpp
+++ lab6v5/main.cpp
@@ -1,21 +1,31 @@
 #include <iostream>
 #include <string>
+#include <stdexcept>
 using namespace std;
 
+int get_ind(string item) {
+    string menu[5] = { "olivie", "cesar", "blinchiki", "kompot", "bulochka"};
+    int i = 0;
+    for (i=0; i<5; i++) {
+        if (menu[i] == item) {
+            goto exit;
+        }
+    }
+    throw invalid_argument( "No such element!" );
+    exit:
+    return i;
+}
+
+
 int main() {
-    enum Menu {
-        olivie=150,
-        cesar=120,
-        blinchiki=80,
-        kompot=20,
-        bulochka=10
-    };
+    string menu[5] = { "olivie", "cesar", "blinchiki", "kompot", "bulochka"};
+    int prices[5] = {100, 200, 80, 20, 10};
     while (true) {
         string item;
         cout << "Введите название блюда (olivie,cesar,blinchiki,kompot,bulochka): ";
         cin >> item;
         if (item=="stop") { break;}
-        
+
 
     }
     return 0;
