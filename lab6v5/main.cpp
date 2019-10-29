5d4ed4273006f5932f667a758a5f38ebcde17003
Nikolai <npv3s@skopa.dev>
2019-10-29 21:11:00 +0300

    Lab6v5 update

--- lab6v5/main.cpp
+++ lab6v5/main.cpp
@@ -1,40 +1,21 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
-#include <vector>
-#include <stdexcept>
 using namespace std;
-
-int get_ind(string item) {
-    string menu[5] = { "olivie", "cesar", "blinchiki", "kompot", "bulochka"};
-    int i = 0;
-    for (i=0; i<5; i++) {
-        if (menu[i] == item) {
-            goto exit;
-        }
-    }
-    throw invalid_argument( "No such element!" );
-    exit:
-    return i;
-}
 
 
 int main() {
-    struct Item {
-        int index;
-        string name;
-        int number;
-    };
+    string menu[5] = { "Оливье", "Цезарь", "Блинчики", "Компот", "Булочка"};
     int prices[5] = {100, 200, 80, 20, 10};
-    vector<Item> items;
-    while (true) {
-        string item;
-        cout << "Введите название блюда (olivie,cesar,blinchiki,kompot,bulochka): ";
-        cin >> item;
-        if (item=="stop") { break;}
-        items.push_back({get_ind(item), item, number});
+    int amount[5];
 
-
-
+    int i;
+    for (i=0; i<5; i++) {
+        cout << "Введите количество блюда \"" << menu[i] << "\": ";
+        cin >> amount[i];
     }
+
+
+
     return 0;
 }
