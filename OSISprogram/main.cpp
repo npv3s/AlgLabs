4ae6aa8ea9640775c537aba2cda4b2579596c36b
Nikolai <npv3s@skopa.dev>
2019-11-02 14:44:11 +0300

    Add loop

--- OSISprogram/main.cpp
+++ OSISprogram/main.cpp
@@ -3,38 +3,43 @@
 using namespace std;
 
 int main() {
-    string arg;
-    cout << "1. ping <arg>\n2. ls <arg>\n3. echo <arg>\n4. mkdir <arg>\n5. tree <arg>\nВыбирите режим работы: ";
-    int mode;
-    cin >> mode;
-    switch (mode) {
-        case 1:
-            cout << "Введите адрес: ";
-            cin >> arg;
-            system(string(string("ping -c 5 ") + arg).c_str());
-            break;
-        case 2:
-            cout << "Введите путь: ";
-            cin >> arg;
-            system(string(string("ls ") + arg).c_str());
-            break;
-        case 3:
-            cout << "Введите текст: ";
-            cin >> arg;
-            system(string(string("echo ") + arg).c_str());
-            break;
-        case 4:
-            cout << "Введите название папки: ";
-            cin >> arg;
-            system(string(string("mkdir /home/npv3s/") + arg).c_str());
-            break;
-        case 5:
-            cout << "Введите путь: ";
-            cin >> arg;
-            system(string(string("tree ") + arg).c_str());
-            break;
-        default:
-            cout << "Команда не опознана!" << endl;
+    while (true) {
+        string arg;
+        cout << "1. ping <arg>\n2. ls <arg>\n3. echo <arg>\n4. mkdir <arg>\n5. tree <arg>\n6. Выйти\nВыбирите режим работы: ";
+        int mode;
+        cin >> mode;
+        switch (mode) {
+            case 1:
+                cout << "Введите адрес: ";
+                cin >> arg;
+                system(string(string("ping -c 5 ") + arg).c_str());
+                break;
+            case 2:
+                cout << "Введите путь: ";
+                cin >> arg;
+                system(string(string("ls ") + arg).c_str());
+                break;
+            case 3:
+                cout << "Введите текст: ";
+                cin >> arg;
+                system(string(string("echo \"") + arg + string("\"")).c_str());
+                break;
+            case 4:
+                cout << "Введите название папки: ";
+                cin >> arg;
+                system(string(string("mkdir /home/npv3s/") + arg).c_str());
+                break;
+            case 5:
+                cout << "Введите путь: ";
+                cin >> arg;
+                system(string(string("tree ") + arg).c_str());
+                break;
+            case 6:
+                return 0;
+            default:
+                cout << "Команда не опознана!" << endl;
+        }
+        cout << endl;
     }
     return 0;
 }
