0c7794d30614f192cbc83f90fb927e08caaa29aa
Nikolai <npv3s@skopa.dev>
2019-11-05 13:02:31 +0300

    Beta lab7v1

--- lab7v1/main.cpp
+++ lab7v1/main.cpp
@@ -1,6 +1,25 @@
+#include <cmath>
 #include <iostream>
+using namespace std;
+
+#define PI 3.14159265
+#define G 9.81
+#define target 10
+#define target_size 1/2
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int v, alpha;
+    float l;
+    while (true) {
+        cout << "Введите скорость: ";
+        cin >> v;
+        cout << "Введите угол: ";
+        cin >> alpha;
+        l = (pow(v, 2)*sin(2*float(alpha)*PI/180)/G);
+        cout << "L: " << l << endl;
+        if ((l<target+target_size) or (l>target-target_size)) {
+            break;
+        }
+    }
     return 0;
 }
