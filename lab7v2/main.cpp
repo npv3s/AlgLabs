d462313f53bcc3bc267b62c533505635ed1484d1
Nikolai <npv3s@skopa.dev>
2019-11-08 12:30:31 +0300

    Update main.cpp

--- lab7v2/main.cpp
+++ lab7v2/main.cpp
@@ -6,11 +6,26 @@ using namespace std;
 
 int main() {
     cout << "Pi counter" << endl;
-    double pi_1 = 0, pi_2 = 0;
-    for (long long int i=0; i<1000000000; i += 2) {
-        pi_1 += double(4)/(1+2*i)-double(4)/(3+2*i);
+    long double pi_1 = 0, pi_2 = 0;
+    for (long long int i=0; i<50000000; i += 1) {
+        pi_1 += double(4)/(1+4*i)-double(4)/(3+4*i);
+        //cout << long(pi_1*10000000000) << " " << 31415926535 << endl;
+        if (long(pi_1*10000000000) == 31415926535) {
+            cout << pi_1 << endl;
+            return 0;
+        }
     }
     cout << "Pi: " << setprecision(11) << pi_1 << endl;
     cout << "Pi: " << setprecision(11) << pi_2 << endl;
     return 0;
 }
+
+строчка
+
+call func
+
+
+
+func:
+    строчка
+    ret
