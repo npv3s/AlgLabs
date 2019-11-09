412e259d11eb1e4b8cb8f2f55407a0f5ef0a356a
Nikolai <npv3s@skopa.dev>
2019-11-09 21:20:25 +0300

    Done pi_2 counter

--- lab7v2/main.cpp
+++ lab7v2/main.cpp
@@ -6,26 +6,28 @@ using namespace std;
 
 int main() {
     cout << "Pi counter" << endl;
-    long double pi_1 = 0, pi_2 = 0;
-    for (long long int i=0; i<50000000; i += 1) {
-        pi_1 += double(4)/(1+4*i)-double(4)/(3+4*i);
-        //cout << long(pi_1*10000000000) << " " << 31415926535 << endl;
-        if (long(pi_1*10000000000) == 31415926535) {
-            cout << pi_1 << endl;
+    long double pi_1 = 1, pi_2 = 0;
+    for (double i=0; i<0; i += 1) {
+        if (i&&1) {
+            pi_1 -= double(1)/((3+2*i)*pow(3, i+1));
+        } else {
+            pi_1 += double(1)/((3+2*i)*pow(3, i+1));
+        }
+        if (pi_1 >= 3.1415926535/pow(12, 0.5)) {
+            cout << pi_1*pow(12, 0.5) << endl;
+            cout << i << endl;
             return 0;
         }
     }
-    cout << "Pi: " << setprecision(11) << pi_1 << endl;
-    cout << "Pi: " << setprecision(11) << pi_2 << endl;
+    // WORKS!!!!!
+    unsigned int val_2 = 1;
+    for (double i; i<10000; i++) {
+        pi_2 += double(1)/(val_2);
+        val_2 += 2;
+        pi_2 -= double(1)/(val_2);
+        val_2 += 2;
+    }
+    cout << "Pi: " << setprecision(11) << pi_1*pow(12, 0.5) << endl;
+    cout << "Pi: " << setprecision(11) << pi_2*4 << endl;
     return 0;
 }
-
-строчка
-
-call func
-
-
-
-func:
-    строчка
-    ret
