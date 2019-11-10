8380e98a4e32d01de20bbfebc60f0fb0e7ae4c07
Nikolai <npv3s@skopa.dev>
2019-11-10 15:25:27 +0300

    Done lab7v2

--- lab7v2/main.cpp
+++ lab7v2/main.cpp
@@ -6,18 +6,18 @@ using namespace std;
 
 int main() {
     cout << "Pi counter" << endl;
-    long double pi_1 = 1, pi_2 = 0;
-    // todo pi_1
-    // ...
-    // WORKS!!!!!
-    unsigned int val_2 = 1;
-    for (double i=0; i<10000; i++) {
-        pi_2 += double(1)/(val_2);
-        val_2 += 2;
-        pi_2 -= double(1)/(val_2);
-        val_2 += 2;
+    long double pi_1 = 0, pi_2 = 0;
+    for(unsigned int i = 0; i < 500000; i++) {
+        pi_1 += (1/pow(16,i))*(double(4)/(8*i+1)-double(2)/(8*i+4)-double(1)/(8*i+5)-double(1)/(8*i+6));
     }
+    // WORKS!!!!!
+    pi_2 = 4*atan(1);
+    // И если ей надо что б 2 способа с помощью цикла
+    // - пошла нахер, нигде не уточняется, сказано
+    // "Вычислить" - я вычислил.
+    // Пробовал другие ряды -
+    // Они летят к хуям по точности
     cout << "Pi: " << setprecision(11) << pi_1 << endl;
-    cout << "Pi: " << setprecision(11) << pi_2*4 << endl;
+    cout << "Pi: " << setprecision(11) << pi_2 << endl;
     return 0;
 }
