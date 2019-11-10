91a59067efbd2b8fa6761389e08ccbbd6d8e56cf
Nikolai <npv3s@skopa.dev>
2019-11-10 19:32:38 +0300

    Update lab7v3

--- lab7/lab7v3/main.cpp
+++ lab7/lab7v3/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // Здравствуйте, формулы по вариантам я на грядущем занятии на доске напишу (с) НМ
     return 0;
 }
