460258bb4ee3225f46a50336c7aee336a6e53b8b
Nikolai <npv3s@skopa.dev>
2019-11-11 14:38:29 +0300

    Done lab7v3

--- lab7/lab7v3/main.cpp
+++ lab7/lab7v3/main.cpp
@@ -7,13 +7,15 @@ using namespace std;
 #define z_end 4
 
 int main() {
-    // Здравствуйте, формулы по вариантам я на грядущем занятии на доске напишу (с) НМ
     // Весна
     float y;
     cout << "y          x   z" << left << endl;
-    for (float x=0; x<=1; x += 0.1) {
+    for (float x=0; x<1.1; x += 0.1) {
         for (float z=z_start; z<=z_end; z++) {
             y = (sqrt(pow(x, 2)+pow(x, 2))/(x+z-3))*cos(x);
+            if (isnan(y) or isinf(y)) {
+                continue;
+            }
             cout << setw(10) << y << " " << setw(3) << x << " " << setw(3) << z << endl;
         }
     }
