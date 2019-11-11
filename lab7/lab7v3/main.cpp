b5db266f9149a3728194d6708abeba76142c9a7a
Nikolai <npv3s@skopa.dev>
2019-11-11 13:55:12 +0300

    Beta lab7v3

--- lab7/lab7v3/main.cpp
+++ lab7/lab7v3/main.cpp
@@ -1,7 +1,22 @@
+#include <cmath>
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+#define z_start 2
+#define z_end 4
 
 int main() {
     // Здравствуйте, формулы по вариантам я на грядущем занятии на доске напишу (с) НМ
+    // Весна
+    float y;
+    cout << "y          x   z" << left << endl;
+    for (float x=0; x<=1; x += 0.1) {
+        for (float z=z_start; z<=z_end; z++) {
+            y = (sqrt(pow(x, 2)+pow(x, 2))/(x+z-3))*cos(x);
+            cout << setw(10) << y << " " << setw(3) << x << " " << setw(3) << z << endl;
+        }
+    }
+
     return 0;
 }
