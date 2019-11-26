7b0add56a87d5202f97c8135ff0e06fa907cfc35
Nikolai <npv3s@skopa.dev>
2019-11-26 13:05:28 +0300

    Beta lab9v5

---
+++ lab9/lab9v5/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    int n1, n2;
+    cout << "Введите высоту матрицы: ";
+    //cin >> n1;
+    cout << "Введите ширину матрицы: ";
+    //cin >> n2;
+    n1 = 10;n2 = 10;cout << endl;
+    srand(unsigned(time(nullptr)));
+    double k = double(4) / RAND_MAX;
+    int a[n1][n2];
+    for (int i = 0; i < n1; i++) {
+        for (int y = 0; y < n2; y++) {
+            a[i][y] = rand() * k;
+        }
+    }
+
+    int max[n1];
+    for (int i = 0; i < n1; i++) {
+        int local_max[n2];
+        for (int y = 0; y < n2; y++) {
+            local_max[y] = 0;
+
+            int prev_max = 0;
+            for (int z = 0; z < n2; z++) {
+                if (a[i][y] == a[i][z]) {
+                    local_max[y]++;
+                }
+            }
+            
+
+        }
+        max[i] = 0;
+        for (int y = 0; y < n2; y++) {
+            if (local_max[y] > max[i]) {
+                max[i] = local_max[y];
+            }
+        }
+    }
+
+    int max_raw = max[0];
+    for (int i = 0; i < n1; i++) {
+        if (max[max_raw] < max[i]) {
+            max_raw = i;
+        }
+
+        for (int y = 0; y < n2; y++) {
+            cout << left << setw(3) << a[i][y];
+        }
+        cout << "|" << max[i] << endl;
+    }
+    cout << endl;
+    cout << "Max raw: " << max_raw+1 << endl;
+
+    return 0;
+}
