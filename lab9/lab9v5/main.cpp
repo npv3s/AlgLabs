6ac6fa9a40212a9a8a468c3fab8a9a07627994f2
Nikolai <npv3s@skopa.dev>
2019-12-02 09:35:35 +0300

    Update lab9v5

--- lab9/lab9v5/main.cpp
+++ lab9/lab9v5/main.cpp
@@ -9,7 +9,9 @@ int main() {
     //cin >> n1;
     cout << "Введите ширину матрицы: ";
     //cin >> n2;
-    n1 = 10;n2 = 10;cout << endl;
+    n1 = 20;
+    n2 = 20;
+    cout << endl;
     srand(unsigned(time(nullptr)));
     double k = double(4) / RAND_MAX;
     int a[n1][n2];
@@ -21,23 +23,32 @@ int main() {
 
     int max[n1];
     for (int i = 0; i < n1; i++) {
-        int local_max[n2];
+        int local_max[n2][2];
+
         for (int y = 0; y < n2; y++) {
-            local_max[y] = 0;
+            local_max[y][0] = 0;
+            local_max[y][1] = 0;
 
-            int prev_max = 0;
-            for (int z = 0; z < n2; z++) {
-                if (a[i][y] == a[i][z] and a[i][z] == a[i][z-1]) {
-                    local_max[y]++;
+            int z;
+            for (z = y; z < n2; z++) {
+                local_max[y][1]++;
+                z++;
+                while (a[i][z] == a[i][z - 1] and z < n2) {
+                    if (a[i][y] == a[i][z]) {
+                        local_max[y][1]++;
+                    }
+                    z++;
                 }
+                if (local_max[y][0] < local_max[y][1]) {
+                    local_max[y][0] = local_max[y][1];
+                }
+                local_max[y][1] = 0;
             }
-
-
         }
         max[i] = 0;
         for (int y = 0; y < n2; y++) {
-            if (local_max[y] > max[i]) {
-                max[i] = local_max[y];
+            if (local_max[y][0] > max[i]) {
+                max[i] = local_max[y][0];
             }
         }
     }
