286b20b58c7935f475376e0ae6103b830724945d
Nikolai <npv3s@skopa.dev>
2019-12-10 12:33:31 +0300

    Update lab10v3

--- lab10/lab10v3/main.cpp
+++ lab10/lab10v3/main.cpp
@@ -1,44 +1,64 @@
 #include <stdio.h>
-#include <cstring>
 
 int main() {
-    //char text[2048];
+    //char text[256];
     //gets(text);
-    char text[2048] = "Hu1. Hu2 Hu3 Hu4.";
-    printf("%s\n", text);
-    char *pointer = strstr(text, ".") + 2;
-    char *point  = strchr(pointer, '.');
-    if (point != nullptr) {
-        *(point) = '\0';
-    }
-
-    int space[2];
-    int spacei = 0;
-    for (int i=0; spacei < 2; i++) {
-        if (pointer[i] == ' ') {
-            space[spacei] = i;
-            spacei++;
+    char text[256] = "Aa. Bb cc dd.";
+    int point[2] = {-1, -1};
+    for (int i=0; i < 256; i++) {
+        if (text[i] == '.') {
+            point[0] = i;
+            break;
         }
     }
+    if (point[0] == -1) { return -1; }
+    for (int i=point[0]+1; i < 256; i++) {
+        if (text[i] == '.') {
+            point[1] = i;
+            break;
+        }
+    }
+    if (point[1] == -1) { return -1; }
+    int space[2] = {-1, -1};
+    for (int i=point[0]+2; i < 256; i++) {
+        if (text[i] == ' ') {
+            space[0] = i;
+            break;
+        }
+    }
+    if (space[0] == -1) { return -1; }
+    for (int i=space[0]+1; i < 256; i++) {
+        if (text[i] == ' ') {
+            space[1] = i;
+            break;
+        }
+    }
+    if (space[1] == -1) { return -1; }
 
-    int end = strlen(pointer);
-    printf("%d %d %d\n", space[0], space[1], end);
-    char word1[space[0]], word2[space[1]-space[0]-1], word3[end-space[1]-1];
-    strcpy(word1, pointer);
-    strcpy(word2, pointer+space[0]+1);
-    strcpy(word3, pointer+space[1]+1);
+    char word1[space[1]-space[0]];
+    char word2[point[1]-space[1]];
 
-    char out[2048] = {'\0'};
-    strcat(out, word1);
-    strcat(out, " ");
-    strcat(out, word3);
-    strcat(out, " ");
-    strcat(out, word2);
-    strcat(out, ".");
+    for (int i=0; i < space[1]-space[0]-1; i++) {
+        word1[i] = text[i+space[0]+1];
+    }
+    word1[space[1]-space[0]-1] = '\0';
 
-    strcpy(pointer, out);
+    for (int i=0; i < point[1]-space[1]-1; i++) {
+        word2[i] = text[i+space[1]+1];
+    }
+    word2[point[1]-space[1]-1] = '\0';
+
+    int i=space[0]+1;
+    for (int y=0; y < space[1]-space[0]-1; y++, i++) {
+        text[i] = word1[y];
+    }
+    text[i] = ' ';
+    i++;
+    for (int y=0; y < point[1]-space[1]-1; y++, i++) {
+        text[i] = word2[y];
+    }
+    text[i] = '.';
 
     printf("%s\n", text);
-
     return 0;
 }
