aa21bdd8a20372a1801141e877426e4998d3fb34
Nikolai <npv3s@skopa.dev>
2020-01-27 12:21:26 +0300

    Add lab13v1

---
+++ lab13/lab13v1/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int distanceFromCenter(int x, int y) {
+    return sqrt(pow(x, 2) + pow(y, 2));
+}
+
+int main() {
+    int radius;
+    //cin >> radius;
+    radius = 3;
+    int x[] = {0, -3, 5, 3, -4, 5, 0, 4, 2, 4};
+    int y[] = {3, -3, -5, 5, 0, -2, 4, -5, 1, 1};
+    unsigned int sum = 0;
+    for(int i = 0; i < 10; i++) {
+        if (distanceFromCenter(x[i], y[i]) <= radius) {
+            sum++;
+        }
+    }
+    cout << sum << endl;
+    return 0;
+}
