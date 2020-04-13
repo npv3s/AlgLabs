5a94f9133b7d94b7f4aff4e2a50402d15720ab1e
Nikolai <npv3s@skopa.dev>
2020-04-13 23:34:39 +0300

    Final lab16++

--- lab16++/main.cpp
+++ lab16++/main.cpp
@@ -7,11 +7,19 @@ using namespace std;
 int main() {
     auto db = openDB("../db.csv");
 
-    char code[CODE_SIZE];
-    cout << "Введите артикул: " << endl;
-    cin >> code;
-    print_by_code(code, db);
-    print_c_code(db);
+    int work_mode;
+    cout << "Выберети режим работы:\n"
+            "1. Вывести одежду с требуемым кодом\n"
+            "2. Вывести детскую одежду" << endl;
+    cin >> work_mode;
+
+    if (work_mode == 1) {
+        char code[CODE_SIZE];
+        cout << "Введите артикул: " << endl;
+        cin >> code;
+        print_by_code(code, db);
+    }
+    if (work_mode == 2) print_c_code(db);
 
     return 0;
 }
